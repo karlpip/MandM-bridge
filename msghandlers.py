9c8d641dd5bc462f703bca08bccccb21f3c06acd
Karl <karl.piplies@protonmail.com>
2023-07-02 13:58:41 +0200

    args

--- msghandlers.py
+++ msghandlers.py
@@ -6,11 +6,11 @@ from bridge import Bridge
 
 
 class MsgHandlers:
-    def murmur_check_botamusique(self, _, sender: str, msg: str) -> Tuple[bool, str]:
+    def murmur_check_botamusique(self, _bridge, sender: str, msg: str) -> Tuple[bool, str]:
         if sender == "botamusique":
             return False, msg
         return True, msg
 
-    def murmur_remove_html(self, _, _, msg: str) -> Tuple[bool, str]:
+    def murmur_remove_html(self, _bridge, _sender, msg: str) -> Tuple[bool, str]:
         msg = re.sub('<a href="(.*)">.*<\\/a>', "\\1", msg)
         return True, msg
